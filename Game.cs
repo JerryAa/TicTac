148728c9b01b507ef651ceca5826c77a3b8f9294
JerryAa <jerrya15@yahoo.com>
2018-08-26 08:10:24 -0500

    Initial commit

---
+++ Game.cs
@@ -0,0 +1,28 @@
+using System; 
+
+namespace TicTac
+{ 
+
+	public interface Status 
+	{ 
+
+		// signatures of game state methods 
+
+	} 
+	public abstract class Game 
+	{ 
+
+		// Game methods, fields, etc 
+
+	} 
+
+	public class Player
+	{ 
+		// player data ie, name, x or o, etc
+
+	
+	} 
+
+
+
+} 
