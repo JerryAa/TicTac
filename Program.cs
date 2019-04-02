6c42011b657ac0fbc13f440cc290a18aaceb8b22
JerryAa <jerrya15@yahoo.com>
2019-04-01 22:44:12 -0500

    Fixed: Both players have acces to board

--- Program.cs
+++ Program.cs
@@ -9,38 +9,34 @@ namespace TicTac
 			Console.WriteLine("Enter name for Player 1:"); 
 
 			string name_p1 = Console.ReadLine(); 
-			Player p1 = new Player(name_p1); 
-			p1.Create(); 
-			
+			Console.WriteLine("\n"); 
 			Console.WriteLine("Enter name for Player 2:"); 
 			string name_p2 = Console.ReadLine(); 
 			
-			Player p2 = new Player(name_p2); 
-			p2.Create(); 
+            Game g = new Player(name_p1, name_p2); 
+            g.Create(); 
+			Console.WriteLine("\n"); 
+            g.Print(); 
+			Console.WriteLine("\n"); 
 
 			string stop = ""; 
 			do { 
 
 				try { 
 					
-					p1.Print(); 
 					Console.WriteLine("Player One's Turn"); 
 					Console.Write("Which position would you like:"); 
 					int pos = Convert.ToInt32(Console.ReadLine()); 
 					Console.WriteLine("\n"); 
-					p1.movesPlayed.Add(pos); 
-					p1.UpdateBoard(pos, Plyr.player1); 
+					g.movesPlayed.Add(pos); 
+					g.UpdateBoard(pos, Plyr.player1); 
 
 					Console.WriteLine("Player Two's Turn"); 
 					Console.Write("Which position would you like:"); 
 					pos = Convert.ToInt32(Console.ReadLine()); 
 					Console.WriteLine("\n"); 
-					p2.UpdateBoard(pos, Plyr.player2); 
-					p2.movesPlayed.Add(pos); 
-
-
-					if (p1.movesPlayed.Count > 3 ||  p2.movesPlayed.Count > 3 )  
-						break; 
+					g.UpdateBoard(pos, Plyr.player2); 
+					g.movesPlayed.Add(pos); 
 
 					Console.WriteLine("\n"); 
 					Console.WriteLine("Continue? Yes or No"); 
